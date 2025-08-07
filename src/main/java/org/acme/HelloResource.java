9df98710f6ac6c75578e95d82cba8b96927a4ace
Ove Klinghed <ove.klinghed@gmail.com>
2025-08-07 17:22:19 +0200

    Mod

--- src/main/java/org/acme/HelloResource.java
+++ src/main/java/org/acme/HelloResource.java
@@ -6,7 +6,7 @@ import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
 @Path("/hello")
-public class GreetingResource {
+public class HelloResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
